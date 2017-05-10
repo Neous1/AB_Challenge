95c1706138d14e5616b0c3eb0112d9b51d8e8f10
Neous1 <Neous1@users.noreply.github.com>
2017-05-10 02:17:25 -0400

    generated multiple characters changed character via a click event

---
+++ vuejs/vuejs_masteringWebApps/Applications/starbase/src/components/Character.vue
@@ -0,0 +1,32 @@
+<<template>
+  <div class="col-md-4" @click="switchCharacter">
+    {{character}}
+  </div>
+</template>
+<<script>
+export default {
+    props:["id"],
+    data(){
+        return{
+            character: {}
+        }
+    },
+    methods:{
+        fetchCharacter(id){
+            fetch(`http://swapi.co/api/people/${id}`, {
+                method:"GET"
+            })
+            .then(response =>response.json())
+            .then(json => this.character = json)
+        },
+        switchCharacter(){
+            let random_id = Math.floor(Math.random()* 83) + 1
+            this.fetchCharacter(random_id)
+        }
+    },
+    created(){
+        this.fetchCharacter(this.id)
+    }
+  
+}
+</script>
